21812b10043b538133dab339362b3d02d0a9502d
Josh Mohrer <joshmohrer@gmail.com>
2014-07-24 13:58:21 -0400

    Add image upload with paperclip

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise', '~> 3.1.0.rc2'
-
+gem 'paperclip', github: 'thoughtbot/paperclip'	
 
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -50,8 +50,7 @@ class PinsController < ApplicationController
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
-
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
